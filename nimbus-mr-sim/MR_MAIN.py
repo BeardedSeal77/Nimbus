bb1f4ee99c87ed68351ef17646227c65d2f4eae3
Koewkie <darrat199@gmail.com>
2025-10-27 15:45:06 +0200

    Update to MR launcher, now multiprocessing instead of threading for better performance

--- nimbus-mr-sim/MR_MAIN.py
+++ nimbus-mr-sim/MR_MAIN.py
@@ -5,6 +5,7 @@ import mr_object_telemetry_subscriber
 import mr_video_subscriber
 import mr_yaw_publisher
 import time
+import multiprocessing
 
 def run_in_thread(target):
     t = threading.Thread(target=target)
@@ -12,19 +13,39 @@ def run_in_thread(target):
     t.start()
     return t
 
+def run_in_process(target):
+    p = multiprocessing.Process(target=target)
+    p.daemon = True
+    p.start()
+    return p
+
 if __name__ == "__main__":
-    threads = [
-        run_in_thread(mr_drone_pose_subscriber.main),
-        run_in_thread(mr_messaging_subscriber.main),
-        run_in_thread(mr_object_telemetry_subscriber.main),
-        run_in_thread(mr_video_subscriber.main),
-        run_in_thread(mr_yaw_publisher.main)
+    # threads = [
+    #     run_in_thread(mr_drone_pose_subscriber.main),
+    #     run_in_thread(mr_messaging_subscriber.main),
+    #     run_in_thread(mr_object_telemetry_subscriber.main),
+    #     run_in_thread(mr_video_subscriber.main),
+    #     # run_in_thread(mr_yaw_publisher.main)
+    # ]
+
+    processes = [
+        run_in_process(mr_drone_pose_subscriber.main),
+        run_in_process(mr_messaging_subscriber.main),
+        run_in_process(mr_object_telemetry_subscriber.main),
+        run_in_process(mr_video_subscriber.main)
     ]
 
     time.sleep(0.5)
     print("All subsystems started. Press Ctrl+C to stop.")
     try:
-        while True:
-            pass  # keep main alive
+        for p in processes:
+            p.join()
     except KeyboardInterrupt:
-        print("\nShutting down all threads...") 
+        print("\nShutting down all processes...")
+        for p in processes:
+            p.terminate()
+    # try:
+    #     while True:
+    #         pass  # keep main alive
+    # except KeyboardInterrupt:
+    #     print("\nShutting down all threads...") 
